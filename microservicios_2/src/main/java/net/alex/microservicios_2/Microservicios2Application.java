4d93c615d081ccd552166d2736d9408533b62326
alexa <alexa@Crow>
2020-07-08 23:59:44 -0500

    subiendo archivo

---
+++ microservicios_2/src/main/java/net/alex/microservicios_2/Microservicios2Application.java
@@ -0,0 +1,13 @@
+package net.alex.microservicios_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicios2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservicios2Application.class, args);
+	}
+
+}
---
+++ microservicios_2/src/test/java/net/alex/microservicios_2/Microservicios2ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.alex.microservicios_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicios2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
